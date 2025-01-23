a3db678dfc56a853046b0544416b1e305f17983d
jhhrnavy <jhhrnavy@gmail.com>
2025-01-23 20:49:01 +0900

    refactor(BlockSpawner): Rename function for clarity
    Renamed function 'SpawnBlock' to 'SpawnNewBlock' in BlockSpawner.cs

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -3,11 +3,30 @@ using UnityEngine;
 public class BlockSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject blockPrefab;
+    [SerializeField] private GameObject currentBlock;
+    [SerializeField] private GameObject prevBlock;
 
-    public void SpawnBlock()
+    public GameObject CurrentBlock { get => currentBlock; }
+    public GameObject PrevBlock { get => prevBlock; }
+
+    public void SpawnNewBlock()
     {
         if (blockPrefab == null) return;
 
-        Instantiate(blockPrefab, transform.position, Quaternion.identity);
+        GameObject block = Instantiate(blockPrefab, transform.position, Quaternion.identity);
+        block.name = "currentBlock";
+        
+        if(currentBlock == null)
+        {
+            currentBlock = block;
+            currentBlock.GetComponent<Collider>().isTrigger = false;
+        }
+        else
+        {
+            currentBlock.name = "prevBlock";
+            prevBlock = currentBlock;
+            block.AddComponent<BlockCollision>();
+            currentBlock = block;
+        }
     }
 }
